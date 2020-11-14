55bc9f959d51178d2687d9a70942f0bfeb1b587b
Simon McCorkindale <simon@funx.jp>
2020-11-14 11:28:08 +0800

    Create script to run in parallel

---
+++ looper.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+i=0
+while [ $i -lt 8141 ]
+    do
+        for j in {1..30};
+            do
+                node inserter.js $i 1998-08-02&
+                i=$((i+1))
+            done
+    wait < <(jobs -p)
+    done
