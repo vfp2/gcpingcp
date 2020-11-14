38683e8f6add84f85a50a747724ca6e9fc6cfa4f
Simon McCorkindale <simon@funx.jp>
2020-11-14 19:44:53 +0800

    Don't print noisy warnings

--- looper.sh
+++ looper.sh
@@ -5,7 +5,7 @@ while [ $i -lt 8141 ]
     do
         for j in {1..6};
             do
-                node inserter.js $i 1998-08-02&
+                node --no-warnings inserter.js $i 1998-08-02&
                 i=$((i+1))
             done
     wait < <(jobs -p)
