f5e26ce063e96dfe7ac15aa619ad040f567321e5
Wandering Consciousness <simon@funx.jp>
2023-08-20 07:54:06 +0800

    Modify BQ insertion looper to insert the latest 3 years of data

--- bqlooper.sh
+++ bqlooper.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 i=0
-while [ $i -lt 8141 ]
+while [ $i -lt 996 ]
     do
         for j in {1..6};
             do
-                node --no-warnings bqinserter.js $i 1998-08-02&
+                node --no-warnings bqinserter.js $i 2020-11-27&
                 i=$((i+1))
             done
     wait < <(jobs -p)
