efb402527f9aa34b0f9a169632a7e94aa46f3d40
Wandering Consciousness <simon@funx.jp>
2023-08-30 00:00:12 +0800

    Update bqlooper.sh

--- bqlooper.sh
+++ bqlooper.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 i=0
-while [ $i -lt 996 ]
+while [ $i -lt 248 ]
     do
         for j in {1..6};
             do
-                node --no-warnings bqinserter.js $i 2020-11-27&
+                node --no-warnings bqinserter.js $i 2022-12-20&
                 i=$((i+1))
             done
     wait < <(jobs -p)
