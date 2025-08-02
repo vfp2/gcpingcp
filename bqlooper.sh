23a393b0cf116b331ce661e0f475e784d8710549
Wandering Consciousness <10329002+Wandering-Consciousness@users.noreply.github.com>
2025-08-02 21:41:22 +1200

    get 2023-2025 data

--- bqlooper.sh
+++ bqlooper.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 i=0
-while [ $i -lt 248 ]
+while [ $i -lt 708 ]
     do
         for j in {1..6};
             do
-                node --no-warnings bqinserter.js $i 2022-12-20&
+                node --no-warnings bqinserter.js $i 2023-08-26&
                 i=$((i+1))
             done
     wait < <(jobs -p)
